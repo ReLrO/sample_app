02847e244264af76f75173591bb00eb51b96d805
Ariel Rosenthal <relrosen@hotmail.com>
2011-06-13 14:52:15 +0300

    Added user following

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$('#micropost_content').live('keyup keydown', function(e) {
+  var maxLen = 140;
+  var left = maxLen - $(this).val().length;
+  $('#char-count').html(left);
+});
